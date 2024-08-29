3edceff3b29a6a9442259843ac609e0a1a901e6d
Richard Shaju <richardshaju66@gmail.com>
2024-08-29 19:35:56 +0530

    Bug fixed: Deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export",images: { unoptimized: true }}
+const nextConfig = { output: "export", images: { unoptimized: true } };
 
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  
-};
-
-
-module.exports = nextConfig
+module.exports = nextConfig;

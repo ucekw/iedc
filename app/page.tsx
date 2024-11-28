8e6b94f0ea56dd749a673f96f79ec880074140d3
Richard Shaju <richardshaju66@gmail.com>
2024-11-28 19:43:23 +0530

    refactor: Remove unused components and files from the project

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import Events from "@/components/events";
 import Footer from "@/components/footer";
 import HeroSection from "@/components/heroSection";
 import Loading from "@/components/loading";
-import Navbar from "@/components/navbar"           ;
 import OurSaga from "@/components/ourSaga";
 import VisionAndMission from "@/components/visionAndMission";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {

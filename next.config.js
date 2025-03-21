6c89836f9be9d17ff1e6725296ea1e7c140cc713
Richard Shaju <richardshaju66@gmail.com>
2025-03-21 21:49:58 +0530

    Fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: 'https://iedc.uck.ac.in',
-  // assetPrefix: 'https://iedc.uck.ac.in',
   images: {
     unoptimized: true,
     remotePatterns: [
